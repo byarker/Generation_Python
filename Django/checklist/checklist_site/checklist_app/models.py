02289ec51f5d4e2ca16290e55e5a3d150e16a0cc
Ben Yarker <byarker42@gmail.com>
2022-04-14 11:25:03 +0100

    setup database model

--- Django/checklist/checklist_site/checklist_app/models.py
+++ Django/checklist/checklist_site/checklist_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    game_title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.game_title
+
+class Check(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    check_text = models.CharField(max_length=200)
+    check_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.check_text
--- Django/checklist/checklist_site/checklist_site/settings.py
+++ Django/checklist/checklist_site/checklist_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'checklist_app.apps.ChecklistAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
